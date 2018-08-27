e30ae409b801be7d1e6dc10b1f0e9609bd81d6cc
Thada Wangthammang <mildronize@gmail.com>
2018-08-27 17:34:52 +0700

    add NextScript

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          {/* <NextScript /> */}
+          <NextScript />
         </body>
       </html>
     )
